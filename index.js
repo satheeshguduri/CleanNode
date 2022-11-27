8367c3963e218d302e016c8d6ad282c5ac1b15ea
satheeshg <satheesh.g@abhibus.com>
2022-11-27 15:24:36 +0530

    get method committed

--- index.js
+++ index.js
@@ -4,6 +4,14 @@ const myApp = express();
 
 const PORT  = 3000
 
+
+myApp.get("/user",(req,res,next)=>{
+
+    let user = {name : "satheesh", tech : "api"}
+    res.send(user)
+})
+
+
 myApp.listen(PORT,()=>{
-    console.log("SERVER RUNNING ON : "+PORT);
+    console.log("SERVER RUNNING ON : "+PORT)
 })
