d72e6693f11545507b38bd72ea0895d7ffd5bbee
satheeshg <satheesh.g@abhibus.com>
2022-11-27 16:53:23 +0530

    post method

--- index.js
+++ index.js
@@ -2,34 +2,16 @@ const express = require("express")
 
 const myApp = express();
 
+
 const PORT  = 3000
 
-// // get method with out params
-// myApp.get("/user",(req,res,next)=>{
+myApp.use(express.json())
+// post method to show the data
 
-//     let user = {name : "satheesh", tech : "api"}
-//     res.send(user)
-// })
+myApp.post("/user",(req,res,next) =>{
+   
+    let _body = req.body
 
-// get method with param
-myApp.get("/user/:id",(req,res,next)=>{
-
-    let _id = req.params.id
-
-    let user_list = [
-            {id : 1, name : "satheesh", tech : "api"},
-            {id : 2, name : "mahesh", tech : "kotlin"},
-            {id : 1, name : "suresh", tech : "java"}
-    ]
-
-    user_list = user_list.filter(x=>x.id==_id)
-    res.send(user_list)
-})
-
-// get method with query
-myApp.get("/user",(req,res,next)=>{
-
-    let _query = req.query
 
     let user_data = [
         {id:101, name : "satheesh", addr: "hyderabad"},
@@ -40,10 +22,12 @@ myApp.get("/user",(req,res,next)=>{
 
     ]
 
-    user_data = user_data.filter(x=>x.id==_query.id & x.name ==_query.name)
+    user_data = user_data.filter(x=>x.id==_body.id & x.name == _body.name)
     res.send(user_data)
 
 })
+
+
 
 myApp.listen(PORT,()=>{
     console.log("SERVER RUNNING ON : "+PORT)
