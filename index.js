a31f453f2bfa77a1731474e9cfb886ec0f198785
satheeshg <satheesh.g@abhibus.com>
2022-11-27 18:14:41 +0530

    put method

--- index.js
+++ index.js
@@ -28,6 +28,29 @@ myApp.post("/user",(req,res,next) =>{
 })
 
 
+// adding put method to update
+
+myApp.put("/user",(req,res,next)=>{
+
+    let _body = req.body
+
+
+    let user_data = [
+        {id:101, name : "satheesh", addr: "hyderabad"},
+        {id:102, name : "mahesh", addr: "pune"},
+        {id:103, name : "suresh", addr: "delhi"},
+        {id:101, name : "satheesh", addr: "bangalore"},
+        {id:102, name : "naresh", addr: "bombay"},
+
+    ]
+
+    let index = user_data.findIndex(x=>x.id==_body.id)
+    if(index!=-1){
+        user_data[index] = _body
+    }
+    res.send(user_data)
+})
+
 
 myApp.listen(PORT,()=>{
     console.log("SERVER RUNNING ON : "+PORT)
